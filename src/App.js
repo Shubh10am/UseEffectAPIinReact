d455507990d5020eddd59299f0fa32a0586a767b
Shubh10am <89800791+Shubh10am@users.noreply.github.com>
2023-10-01 16:28:34 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import UseEffectAPI from "./Components/UseEffectAPI";
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>List Of Github Users</h1>
+      <UseEffectAPI />
+    </div>
+  );
+}
